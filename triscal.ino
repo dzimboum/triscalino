d6936f26323adb9ea0d981177f712af3f6be5715
dzimboum <ihave@noma.il>
2012-03-31 00:52:58 +0200

    Initial implementation of pieces
    Board size is 8x17; virtual walls are added in order to prevent
    pieces from escaping the board.

--- triscal.ino
+++ triscal.ino
@@ -1,6 +1,6 @@
 /*
  * Tetris game on a Deuligne 16x2 LCD display
- * 
+ *
  * https://github.com/dzimboum/triscalino
  * Copyright 2012 dzimboum
  * Released under the WTFPL 2.0 license
@@ -8,6 +8,10 @@
  *
  * This is a work in progress, please be patient!
  */
+
+extern "C" {
+  #include <inttypes.h>
+}
 
 #include <Wire.h>
 #include <Deuligne.h>
@@ -22,7 +26,7 @@ Deuligne lcd;
  */
 
 // Upper square
-byte uppperSquare[] = {
+byte upperSquare[] = {
   B11111,
   B11111,
   B11111,
@@ -42,21 +46,233 @@ byte lowerSquare[] = {
   B11111,
   B11111,
   B11111,
-  B11111 
+  B11111
 };
+
+// Upper and lower squares
+byte bothSquares[] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
+
+/*
+ * In order to draw pieces, we have to keep track of pieces
+ * which have already been inserted.  The simplest solution
+ * is to use a 4x16 grid.
+ * But we will add two virtual rows at the top and bottom,
+ * and one virtual column at the right.  Eventually we
+ * need an 8x17 array.
+ */
+boolean board[8][17] = {
+};
+
+// Piece number currently processed
+uint8_t currentNumber = -1;
+
+// Geometric shape
+class Shape
+{
+public:
+  // Geometric shape, it contains a 3x3 grid
+  boolean grid_[3][3];
+
+  /*
+   * In order to speed up processing, we use 3 auxiliary tables:
+   * top_, bottom and right_.  For instance with this shape:
+   *   xoo
+   *   xxo
+   *   oxo
+   * top_    = { 0, 1, 3 }
+   * bottom_ = { 1, 2, 3 }
+   * right_  = { 0, 1, 1 }
+   */
+  unsigned char top_[3];
+  unsigned char bottom_[3];
+  unsigned char right_[3];
+
+public:
+  Shape(unsigned int row0, unsigned int row1, unsigned int row2);
+  boolean pixel(int i, int j);
+};
+
+Shape::Shape(unsigned int row0, unsigned int row1, unsigned int row2)
+{
+  grid_[0][0] = (row0 >> 2) & 1; grid_[0][1] = (row0 >> 1) & 1; grid_[0][2] = row0 & 1;
+  grid_[1][0] = (row1 >> 2) & 1; grid_[1][1] = (row1 >> 1) & 1; grid_[1][2] = row1 & 1;
+  grid_[2][0] = (row2 >> 2) & 1; grid_[2][1] = (row2 >> 1) & 1; grid_[2][2] = row2 & 1;
+
+  for (int i = 0; i < 3; ++i) {
+    top_[i] = bottom_[i] = right_[i] = 3;
+  }
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      if (grid_[i][j]) {
+        bottom_[j] = i;
+        right_[i] = j;
+      }
+      if (grid_[2-i][j]) 
+        top_[j] = 2-i;
+    }
+  }
+}
+
+boolean
+Shape::pixel(int i, int j)
+{
+  return grid_[i][j];
+
+}
+
+Shape allPieces[] = {
+  Shape(
+     B111,
+     B000,
+     B000),
+  Shape(
+     B010,
+     B111,
+     B010),
+  Shape(
+     B110,
+     B110,
+     B000),
+  Shape(
+     B010,
+     B010,
+     B011),
+};
+
+class Piece
+{
+private:
+public:
+  uint8_t row_;
+  uint8_t column_;
+  uint8_t number_;
+  uint8_t oldRow_;
+  uint8_t oldColumn_;
+public:
+  Piece(uint8_t row, uint8_t colonne, uint8_t number);
+  void display();
+  boolean moveUp();
+  boolean moveDown();
+  boolean moveRight();
+  static Piece newPiece();
+};
+
+Piece::Piece(uint8_t row, uint8_t colonne, uint8_t number) :
+    row_(row),
+    column_(colonne),
+    number_(number)
+{
+}
+
+Piece
+Piece::newPiece()
+{
+  ++currentNumber;
+  if (currentNumber >= sizeof(allPieces)/sizeof(Shape))
+    currentNumber = 0;
+  return Piece(3, 0, currentNumber);
+}
+
+void
+Piece::display()
+{
+  if (oldRow_ == row_ && oldColumn_ == column_) return;
+
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      if (allPieces[number_].pixel(i, j)) {
+        lcd.setCursor(oldColumn_+j, (oldRow_+i) / 2 - 1);
+        lcd.print(' ');
+      }
+    }
+  }
+  for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < 3; ++i) {
+      if (allPieces[number_].pixel(i, j)) {
+        lcd.setCursor(column_+j, (row_+i) / 2 - 1);
+        if ((row_ + i) % 2) {
+          lcd.write(1);
+        } else {
+          if (i < 2 && allPieces[number_].pixel(i+1, j)) {
+            lcd.write(2);
+          } else {
+            lcd.write(0);
+          }
+          ++i;
+        }
+      }
+    }
+  }
+}
+
+boolean
+Piece::moveUp()
+{
+  oldRow_   = row_;
+  oldColumn_ = column_;
+  --row_;
+  if ((allPieces[number_].top_[0] != 3 && board[row_ + allPieces[number_].top_[0]][column_]) ||
+      (allPieces[number_].top_[1] != 3 && board[row_ + allPieces[number_].top_[1]][column_]) ||
+      (allPieces[number_].top_[2] != 3 && board[row_ + allPieces[number_].top_[2]][column_])) {
+    ++row_;
+    return false;
+  }
+  return true;
+}
+
+boolean
+Piece::moveDown()
+{
+  oldRow_   = row_;
+  oldColumn_ = column_;
+  ++row_;
+  if ((allPieces[number_].bottom_[0] != 3 && board[row_ + allPieces[number_].bottom_[0]][column_]) ||
+      (allPieces[number_].bottom_[1] != 3 && board[row_ + allPieces[number_].bottom_[1]][column_]) ||
+      (allPieces[number_].bottom_[2] != 3 && board[row_ + allPieces[number_].bottom_[2]][column_])) {
+    --row_;
+    return false;
+  }
+  return true;
+}
+
+boolean
+Piece::moveRight()
+{
+  oldRow_   = row_;
+  oldColumn_ = column_;
+  ++column_;
+  if ((allPieces[number_].right_[0] != 3 && board[row_][column_ + allPieces[number_].right_[0]]) ||
+      (allPieces[number_].right_[1] != 3 && board[row_][column_ + allPieces[number_].right_[1]]) ||
+      (allPieces[number_].right_[2] != 3 && board[row_][column_ + allPieces[number_].right_[2]])) {
+    --column_;
+    return false;
+  }
+  return true;
+}
+
+/* Current piece */
+Piece currentPiece = Piece::newPiece();
 
 int key=-1;
 int oldkey=-1;
-int ligneCurseur = 0;
-int colonneCurseur = 0;
 
 void setup()
 {
   Wire.begin();
   lcd.init();
 
-  lcd.createChar(0, uppperSquare);
+  lcd.createChar(0, upperSquare);
   lcd.createChar(1, lowerSquare);
+  lcd.createChar(2, bothSquares);
 
   lcd.backLight(true); // Backlight ON
 
@@ -66,6 +282,16 @@ void setup()
   lcd.print("ok");
   delay(300);
   lcd.clear();
+
+  for (int j = 0; j < 17; ++j) {
+    board[0][j] = true;
+    board[1][j] = true;
+    board[6][j] = true;
+    board[7][j] = true;
+  }
+  for (int i = 0; i < 8; ++i) {
+    board[i][16] = true;
+  }
 }
 
 void loop() {
@@ -78,32 +304,17 @@ void loop() {
     if (key != oldkey)				
     {
       oldkey = key;
-      lcd.setCursor(colonneCurseur, ligneCurseur / 2);
-      lcd.print(' ');      
       if (key == 0) {
-        ++colonneCurseur;
+        currentPiece.moveRight();
       } else if (key == 1) {
-        --ligneCurseur;
+        currentPiece.moveUp();
       } else if (key == 2) {
-        ++ligneCurseur;
+        currentPiece.moveDown();
       } else if (key == 3) {
-        --colonneCurseur;
+        /* */
       }
  
-      if (ligneCurseur < 0) {
-        ligneCurseur = 0;
-      }
-      if (ligneCurseur > 3) {
-        ligneCurseur = 3;
-      }
-      if (colonneCurseur < 0) {
-        colonneCurseur = 0;
-      }
-      if (colonneCurseur > 15) {
-        colonneCurseur = 15;
-      }
-      lcd.setCursor(colonneCurseur, ligneCurseur / 2);
-      lcd.write(ligneCurseur % 2);
+      currentPiece.display();
     }
   }
 }
